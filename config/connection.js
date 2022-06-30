5faff71a4e98fd3968d5ee8231dd306f66b60043
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-06-30 10:37:57 -0700

    added coneection.js file

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+//unpacking "connect" and "connection" from mongoose
+const { connect, connection } = require('mongoose');
+
+// Using environment variable when it exists. Otherwise use locally stored DB
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/ntrsDB';
+
+connect(connectionString, {//Mongoose 6 always behaves as if useNewUrl... and useUnified... are true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
--- index.js
+++
@@ -1,5 +0,0 @@
-
-const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
-  
-
-console.log(findOdd([1,2,3,2,1]))
